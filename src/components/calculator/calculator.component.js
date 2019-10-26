9e08709b5b4ff28de8fb231da46459dc1ecda424
Eugene <eugenedatsmee@gmail.com>
2019-10-27 00:54:38 +1300

    added repayment schedule and design

--- src/components/calculator/calculator.component.js
+++ src/components/calculator/calculator.component.js
@@ -35,35 +35,42 @@ class Calculator extends Component {
     render() {
         const {loanAmount, loanDuration} = this.state;
         return(
-            <div className="App-header">
                 <div className="Calculator">
-                    <div className="Input-wrapper">
-                        <h4>${loanAmount.toLocaleString('en')}</h4>                  
-                        <small>I want to borrow</small>
-                        <InputRange
-                            step={100}
-                            maxValue={100000}    
-                            minValue={100}
-                            value={loanAmount}
-                            onChange={this.handleAmountChange}
-                        />
-                    </div>
-                    <div  className="Input-wrapper">
-                        <h4>{loanDuration} year{loanDuration > 1 && "s"}</h4>
-                        <small>Over </small>
-                        <InputRange
-                            step={1}
-                            maxValue={30}
-                            minValue={1}
-                            value={loanDuration}
-                            onChange={this.handleDurationChange}
-                        />
-                    </div>
-                    <div className="Input-wrapper">
-                        <MonthlyRepayment years={ loanDuration } amount={loanAmount} />
+                    <div className="Calculator__inner">
+                        <div className="Repayment-schedule">
+                            <div className="Repayment-schedule__weekly">Weekly</div>
+                            <div className="Repayment-schedule__fortnightly">Fornightly</div>
+                            <div className="Repayment-schedule__monthly">Monthly</div>  
+                        </div>
+                        <div className="Input-wrapper">
+                            <div className="Input-wrapper__inner">
+                                <small>I want to borrow</small>
+                                <h4>${loanAmount.toLocaleString('en')}</h4>                  
+                                <InputRange
+                                    step={100}
+                                    maxValue={100000}    
+                                    minValue={100}
+                                    value={loanAmount}
+                                    onChange={this.handleAmountChange}
+                                />
+                            </div>
+                            <div  className="Input-wrapper__inner">
+                                <small>Over </small>
+                                <h4>{loanDuration} year{loanDuration > 1 && "s"}</h4>
+                                <InputRange
+                                    step={1}
+                                    maxValue={30}
+                                    minValue={1}
+                                    value={loanDuration}
+                                    onChange={this.handleDurationChange}
+                                />
+                            </div>
+                            <div className="Input-wrapper__inner">
+                                <MonthlyRepayment years={ loanDuration } amount={loanAmount} />
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
         );
     }
 }
