e5d752f1f894361ee8e9329f9f4bef2ff7c8dcca
eugene <eugenepdm@gmail.com>
2020-06-18 15:00:48 +0800

    update

--- src/components/calculator/loan-duration__input-range.js
+++ src/components/calculator/loan-duration__input-range.js
@@ -31,8 +31,6 @@ class LoanDurationInputRange extends Component {
     console.log(maxTerm);
     const { children } = this.props;
 
-    console.log();
-
     return (
       <Fragment>
         {children}
